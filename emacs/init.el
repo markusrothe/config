5899d85808649d29936120b896de59b27ff8fbc9
Markus Rothe <rothe@e-laborate.net>
2023-06-14 14:46:16 +0200

    Update config

--- emacs/init.el
+++ emacs/init.el
@@ -326,3 +326,16 @@
 
 ;; syntax highlighting for cmake files
 (use-package cmake-mode)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(tooltip ((t (:inherit default :background "#282828" :foreground "white")))))
